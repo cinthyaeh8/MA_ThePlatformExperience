cac01e78f24c8a88c476aea28c5f77d7dda9d81e
cinthyaeh8 <m0933053@o365.fcu.edu.tw>
2021-06-17 11:41:31 +0800

    Version 6.17

---
+++ Platform EXP/Assets/MusicSensor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicSensor : MonoBehaviour
+{
+    public Animator MusicAnimator1;
+    
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            MusicAnimator1.SetBool("Reproduc1", true);
+            print("Reproduc1...");
+
+        }
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            MusicAnimator1.SetBool("Reproduc1", false);
+            print("Reproduc1...");
+
+        }
+
+    }
+}
---
+++ Platform EXP/Assets/SphereSensor.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereSensor : MonoBehaviour
+{
+    public Animator LightAnimator1;
+    public Animator LightAnimator2;
+    public Animator LightAnimator3;
+    public Animator LightAnimator4;
+    public Animator LightAnimator5;
+    public Animator LightAnimator6;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Balls"))
+        {
+            LightAnimator1.SetBool("BLights", true);
+            print("BLights...");
+
+            LightAnimator2.SetBool("BLights", true);
+            print("Blights...");
+
+            LightAnimator3.SetBool("BLights", true);
+            print("BLights...");
+
+            LightAnimator4.SetBool("BLights", true);
+            print("BLights...");
+
+            LightAnimator5.SetBool("BLights", true);
+            print("BLights...");
+
+            LightAnimator6.SetBool("BLights", true);
+            print("BLights...");
+        }
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Balls"))
+        {
+            LightAnimator1.SetBool("BLights", false);
+            print("BLights...");
+
+            LightAnimator2.SetBool("BLights", false);
+            print("Blights...");
+
+            LightAnimator3.SetBool("BLights", false);
+            print("BLights...");
+
+            LightAnimator4.SetBool("BLights", false);
+            print("BLights...");
+
+            LightAnimator5.SetBool("BLights", false);
+            print("BLights...");
+
+            LightAnimator6.SetBool("BLights", false);
+            print("BLights...");
+        }
+
+    }
+}
